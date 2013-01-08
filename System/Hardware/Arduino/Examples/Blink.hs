b3777388ec6ace5c05b387b1db6a99f2189cc6b4
Levent Erkok <erkokl@gmail.com>
2013-01-08 00:21:18 -0800

    more comments

--- System/Hardware/Arduino/Examples/Blink.hs
+++ System/Hardware/Arduino/Examples/Blink.hs
@@ -1,5 +1,5 @@
 -- The mandatory "blink" example on Arduino
---
+
 module System.Hardware.Arduino.Examples.Blink where
 
 import Control.Monad           (forever, void)
@@ -13,6 +13,14 @@ import System.Process (system)
 sleep :: Int -> IO ()
 sleep n = void $ system $ "sleep " ++ show n
 
+-- NB1. Make sure your Arduino is connected to a USB port
+--      Replace the path to the USB device below accordingly
+-- NB2. Make sure to "upload" the Firmata (v2.3) on Arduino
+--      before running this program.
+--
+-- If things don't work, try changing the 'False' below
+-- to 'True' in the call to 'withArduino', which will spit
+-- out a bunch of debug messages. Hopefully, it'll be useful.
 blink :: IO ()
 blink = withArduino False "/dev/cu.usbmodemfd131" go
   where led = pin 13
