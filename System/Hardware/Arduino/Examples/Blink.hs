91547a110259df611c5528cf6a9caadce69117ac
Levent Erkok <erkokl@gmail.com>
2013-01-08 20:37:19 -0800

    simplify a bit

--- System/Hardware/Arduino/Examples/Blink.hs
+++ System/Hardware/Arduino/Examples/Blink.hs
@@ -24,11 +24,10 @@ sleep n = void $ system $ "sleep " ++ show n
 blink :: IO ()
 blink = withArduino False "/dev/cu.usbmodemfd131" go
   where led = pin 13
-        go arduino = forever $ do
-             setPinMode arduino led OUTPUT
-             putStrLn "ON"
-             digitalWrite arduino led True
-             sleep 1
-             putStrLn "OFF"
-             digitalWrite arduino led False
-             sleep 1
+        go arduino = do
+                setPinMode arduino led OUTPUT
+                forever $ do putStr "."
+                             digitalWrite arduino led True
+                             sleep 1
+                             digitalWrite arduino led False
+                             sleep 1
