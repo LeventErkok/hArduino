63ffc875d8201d56cbf8e88fe376306e1125ecab
Levent Erkok <erkokl@gmail.com>
2013-01-30 23:37:38 -0800

    Add counter example

---
+++ System/Hardware/Arduino/Examples/Count.hs
@@ -0,0 +1,41 @@
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  System.Hardware.Arduino.Examples.Count
+-- Copyright   :  (c) Levent Erkok
+-- License     :  BSD3
+-- Maintainer  :  erkokl@gmail.com
+-- Stability   :  experimental
+--
+-- Demonstrates using two switches to count up/down, controlled from
+-- different Haskell threads
+-------------------------------------------------------------------------------
+
+module System.Hardware.Arduino.Examples.Count where
+
+import Control.Monad.Trans (liftIO)
+
+import System.Hardware.Arduino
+
+-- | Two switches, wired up very similar to the set up
+-- in "System.Hardware.Arduino.Examples.Switch", one on pin 2 ('bDown'),
+-- second on pin 4 ('bUp'); both with explicit resistors.
+--
+-- We will keep a counter, incrementing it by 1 if 'bUp'
+-- is pressed; and decrementing it if 'bDown' is pressed;
+-- thus implementing a simple counter.
+counter :: IO ()
+counter = withArduino False "/dev/cu.usbmodemfd131" $ do
+            setPinMode bUp INPUT
+            setPinMode bDown INPUT
+            update (0::Int)
+ where bUp   = pin 4
+       bDown = pin 2
+       update curVal = do
+                liftIO $ print curVal
+                [up, down] <- waitAnyHigh [bUp, bDown]
+                let newVal = case (up, down) of
+                               (True,  True)  -> curVal    -- simultaneous press
+                               (True,  False) -> curVal+1
+                               (False, True)  -> curVal-1
+                               (False, False) -> curVal    -- can't happen
+                update newVal
