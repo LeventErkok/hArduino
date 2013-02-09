418209d6cec22c11195f008734b6f4defd6bb560
Levent Erkok <erkokl@gmail.com>
2013-02-09 13:03:43 -0800

    Add blink schematics

--- System/Hardware/Arduino/SamplePrograms/Blink.hs
+++ System/Hardware/Arduino/SamplePrograms/Blink.hs
@@ -23,7 +23,9 @@ import System.Hardware.Arduino
 -- Note that you do not need any other components to run this example: Just hook
 -- up your Arduino to the computer and make sure StandardFirmata is running on it.
 -- However, you can connect a LED between Pin13 and GND if you want to blink an
--- external led.
+-- external led, like so:
+--
+--  <<http://github.com/LeventErkok/hArduino/raw/master/System/Hardware/Arduino/SamplePrograms/Schematics/Blink.png>>
 blink :: IO ()
 blink = withArduino False "/dev/cu.usbmodemfd131" $ do
            liftIO $ hSetBuffering stdout NoBuffering
