4ea7c2c9a48bdf6e8639879865ee5a8fa850e323
Levent Erkok <erkokl@gmail.com>
2015-09-26 16:10:05 -0700

    Make cabal check happy

---
+++ Setup.hs
@@ -0,0 +1,18 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Main
+-- Copyright   :  (c) Levent Erkok
+-- License     :  BSD3
+-- Maintainer  :  erkokl@gmail.com
+-- Stability   :  experimental
+--
+-- Setup module for the hArduino library
+-----------------------------------------------------------------------------
+
+{-# OPTIONS_GHC -Wall #-}
+module Main(main) where
+
+import Distribution.Simple
+
+main :: IO ()
+main = defaultMain
